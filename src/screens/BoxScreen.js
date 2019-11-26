00855cde844468b3689fd6d46fa978a7ee6a4224
unknown <dcorteswebdev@gmail.com>
2019-11-25 21:30:18 -0600

    feat: Finished styling stuff

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,22 +4,39 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 const BoxScreen = () => {
    return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
+      <View style={styles.chalStyle}>
+        <View style={{ height: 100, width: 100, backgroundColor: 'rgb(000, 000, 255)'}}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'rgb(000, 255, 000)', top: 100}}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'rgb(255, 000, 000)'}}/>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  chalStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
-    height: 200,
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
+    height: 200
   },
-  textStyle: {
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: 'red'
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    fontSize: 18,
+    ...StyleSheet.absoluteFillObject
+  },
+  textThreeStyle: {
     borderWidth: 3,
     borderColor: 'red'
   }
